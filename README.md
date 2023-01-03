67af9b38fcdc8c35174d41336873dd41de6392dd
wjsxqs <xsmlcp@163.com>
2023-01-03 18:27:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# SWAP V2 SDK
+
+Forked from the [Uniswap V2 SDK](https://github.com/Uniswap/v2-sdk).
+
+You can refer to the Uniswap SDK documentation [uniswap.org](https://docs.uniswap.org/sdk/v2/overview).
+
+## Running tests
+
+To run the tests, follow these steps. You must have at least node v10 and [yarn](https://yarnpkg.com/) installed.
+
+Install dependencies
+
+```sh
+yarn install
+```
+
+Run tests
+
+```sh
+yarn test
+```
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x826a5F26A1ac1E05788CcD2eFD4A3207BEF5EC01'
+
+export const INIT_CODE_HASH = '0x35d1f69816e89b5d2e0bf65d314ee21f4fed502d5eeea660638eaba8aadbc6d6'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)

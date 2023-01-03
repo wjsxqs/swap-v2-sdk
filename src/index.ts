54170caced1e35220440e2c48bbb5a1bf38dab88
wjsxqs <xsmlcp@163.com>
2023-01-03 18:41:47 +0800

    Add sdk-core export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export { FACTORY_ADDRESS, INIT_CODE_HASH, MINIMUM_LIQUIDITY } from './constants'
 export * from './errors'
 export * from './entities'
 export * from './router'
+export * from './constants'
+
+export * from '@uniswap/sdk-core'
